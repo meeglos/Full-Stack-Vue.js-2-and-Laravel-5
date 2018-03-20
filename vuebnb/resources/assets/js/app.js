ed5514d428007f55b94e557b332317df1003a130
Miguel Rodriguez <miglos@hotmail.es>
2018-03-20 10:54:19 +0100

    Home page in a row

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -1,97 +1,17 @@
+import App from '../components/App.vue';
 import "core-js/fn/object/assign";
 import Vue from 'vue';
-import { populateAmenitiesAndPrices } from './helpers';
-// import sample from './data';
-let model = JSON.parse(window.vuebnb_listing_model);
-model = populateAmenitiesAndPrices(model);
 
-Vue.component('image-carousel', {
-	props: ['images'],
-	template: `
-		<div class="image-carousel">
-			<img :src="image">
-			<div class="controls">
-				<carousel-control 
-					dir="left" 
-					@change-image="changeImage"
-				></carousel-control>
-				<carousel-control 
-					dir="right" 
-					@change-image="changeImage"
-				></carousel-control>
-			</div>
-		</div>
-	`,
-	data() {
-		return {
-			index: 0
-		}
-	},
-	methods: {
-		changeImage(val) {
-			let newVal = this.index + parseInt(val);
-			if (newVal < 0) {
-				this.index = this.images.length -1;
-			} else if (newVal === this.images.length) {
-				this.index = 0;
-			} else {
-				this.index = newVal;
-			}
-		}
-	},
-	computed: {
-		image() {
-			return this.images[this.index];
-		}
-	},
-	components: {
-		'carousel-control': {
-			template: `<i :class="classes" @click="clicked"></i>`,
-			props: [ 'dir' ],
-			computed: {
-				classes() {
-					return 'carousel-control fa fa-2x fa-chevron-' + this.dir;
-				}
-			},
-			methods: {
-				clicked() {
-					this.$emit('change-image', this.dir === 'left' ? -1 : 1);
-				}
-			}
-		}
-	}
-});
+// import ListingPage from '../components/ListingPage.vue';
+import router from './router'
 
 var app = new Vue({
 	el: '#app',
-	data: Object.assign(model, {
-		headerImageStyle: {
-			'background-image': `url(${model.images[0]})`
-		},
-		contracted: true,
-		modalOpen: false
-	}),
-	methods: {
-		escapeKeyListener(evt) {
-			if (evt.keyCode === 27 && this.modalOpen) {
-				this.modalOpen = false;
-			}
-		}
-	},
-	watch: {
-		modalOpen: function () {
-			var className = 'modal-open';
-			if (this.modalOpen) {
-				document.body.classList.add(className);
-			} else {
-				document.body.classList.remove(className);
-			}
-		}
-	},
-	created: function() {
-		document.addEventListener('keyup', this.escapeKeyListener);
-	},
-	destroyed: function() {
-		document.removeEventListener('keyup', this.escapeKeyListener);
-	}
+	render: h => h(App),
+	router
 });
+
+routes: [
+	{ path: '/', component: HomePage, name: 'home' },
+	{ path: '/listing/:listing', component: ListingPage, name: 'listing' }
+]
