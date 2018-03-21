cb68c38ed6c8c8544bda53edd9a630baff4820dd
Miguel Rodriguez <miglos@hotmail.es>
2018-03-21 11:45:23 +0100

    Finishing lesson 7

--- vuebnb/resources/assets/js/app.js
+++ vuebnb/resources/assets/js/app.js
@@ -2,12 +2,10 @@ import App from '../components/App.vue';
 import "core-js/fn/object/assign";
 import Vue from 'vue';
 
-import App from '../components/App.vue';
-// import ListingPage from '../components/ListingPage.vue';
 import router from './router'
 
 var app = new Vue({
 	el: '#app',
 	render: h => h(App),
 	router
-
+});
